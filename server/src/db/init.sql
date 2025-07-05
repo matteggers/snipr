55815f99353ad85a18015ac2792262def21a3fa0
matteggers <94013259+matteggers@users.noreply.github.com>
2025-07-05 16:39:08 -0400

    Added columns to default db

--- server/src/db/init.sql
+++ server/src/db/init.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS articles (
     if SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    first_paragraph TEXT NOT NULL,
     content TEXT NOT NULL,
     likes INT DEFAULT 0,
     dislikes INT DEFAULT 0,
+    read_later INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT NOW()
 );
